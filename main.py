2256db5da1c28bf5adb60ecd5d280b4c896e5098
Jason Xue <jasonxue1228@gmail.com>
2022-05-29 22:50:27 -0700

    Added title

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from routers.images import router as images_router
 
-app = FastAPI()
+app = FastAPI(title="Asciify Image Service")
 
 app.include_router(images_router)
 
